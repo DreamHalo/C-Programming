73b7bb23d9e220478f8b0b970ca86775f28d5e24
Dream <freddypaw747@gmail.com>
2023-05-19 16:03:23 +0500

    C-NT

---
+++ 01.12.2022/dtnamic.c
@@ -0,0 +1,105 @@
+#include <stdio.h>            
+#include <math.h>
+#include <stdlib.h> 
+#include <time.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <string.h>
+
+
+
+int main(){
+//     int n,a=0;
+//     scanf("%d",&n);
+//      int *ptr=(int *)malloc(n*sizeof(int));
+
+//      for (int i=0 ; i<n;i++){
+//         printf("%d\n",&ptr[i]);
+//      }
+//      for(int i=0; i<n;i++){
+//         scanf("%d",&ptr[i]);
+//         a+=ptr[i];
+//      }
+//      printf("%d",a);
+
+
+//     int  n;
+//     scanf("%d",&n);
+//     int *ptr=(int *)malloc(n*sizeof(int));
+//     for(int i=0; i<n;i++){
+//         scanf("%d",&ptr[i]);
+        
+//      } 
+//         int b=ptr[0];
+//         int c=ptr[0];
+//      for(int j=0;j<n;j++){
+//             if(ptr[j]>b){
+//             b=ptr[j];
+             
+//          }
+//           if(ptr[j]<c){
+//             c=ptr[j];
+             
+//          }
+//     }
+//     printf("%d\n",b);
+//     printf("%d",c+b);
+
+    
+//     int n;
+// scanf ("%d",&n);
+// int *ptr = (int *)calloc(n,sizeof(int));
+
+// int son,b;
+
+// for (int i=0; i<n; i++){
+//     scanf ("%d",&ptr[i]);
+// }
+// son=ptr[0];
+
+// for (int i=1; i<n; i++){
+//     if (ptr[i]<son){
+//         b++;
+//     }else {
+//         son=ptr[i];
+//     }
+  
+// }
+
+
+
+
+
+
+
+//     int  n;
+//     scanf("%d",&n);
+//     int *ptr=(int *)malloc(n*sizeof(int));
+//     for(int i=0; i<n;i++){
+//         scanf("%d",&ptr[i]);
+        
+//      } 
+//         int b=ptr[0];
+//         int c=ptr[0];
+//      for(int j=0;j<n;j++){
+//             if(ptr[j]>b){
+//             b=ptr[j];
+             
+//          }
+//           if(ptr[j]<c){
+//             c=ptr[j];
+             
+//          }
+//     }
+//     printf("%d\n",b);
+//     printf("%d",c);
+
+
+
+
+
+
+
+
+
+}
